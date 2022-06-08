5fe0597620612ebc131e00579387bac6db18f319
Brando Miranda <miranda9@ibm.com>
2022-06-08 15:20:08 -0400

    current progress in coq_serapy with my own dummy debug proj ala proverbot9001 scrape json files

--- brandos_coq_serapy_install.sh
+++ brandos_coq_serapy_install.sh
@@ -1,7 +1,50 @@
+# - install python: https://stackoverflow.com/questions/49118277/what-is-the-best-way-to-install-conda-on-macos-apple-mac
+# install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#  install wget to get miniconda
+brew install wget
+
+# - install miniconda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O ~/miniconda.sh
+bash ~/miniconda.sh -b -p $HOME/miniconda
+# source /Users/my_username/opt/anaconda3/bin/activate
+source ~/miniconda/bin/activate
+conda init zsh
+# conda init
 conda update -n base -c defaults conda
 conda install conda-build
 
 conda create -n coq_serapy python=3.9
 conda activate coq_serapy
+#conda remove --name metalearning2 --all
 
+# - install opam
+brew install opam
+# https://stackoverflow.com/questions/72522266/how-does-one-install-opam-with-conda-for-mac-apple-os-x
+# conda install -c conda-forge opam
+opam init
+# if doing local env? https://stackoverflow.com/questions/72522412/what-does-eval-opam-env-do-does-it-activate-a-opam-environment
+#eval $(opam env)
+
+# - install coq: see https://stackoverflow.com/questions/71117837/how-does-one-install-a-new-version-of-coq-when-it-cannot-find-the-repositories-i
+# local install
+#opam switch create . 4.12.1
+#eval $(opam env)
+#opam repo add coq-released https://coq.inria.fr/opam/released
+#opam install coq
+
+# If you want a single global (wrt conda) coq installation (for say your laptop):
+opam switch create 4.12.1
+opam switch 4.12.1
+opam repo add coq-released https://coq.inria.fr/opam/released
+opam install coq
+
+# - install coq-serapi
+opam install coq-serapi
+
+# - install utop
+opam install utop
+
+# - coq_serapy installs
 pip install -r requirements.txt
+pip install -e ~/coq_serapy
---
+++ debug_proj/debug1_n_plus_1_less_than_n.v
@@ -0,0 +1,11 @@
+Require Import Omega.
+
+ Theorem t:
+    forall n: nat, 1 + n > n.
+ Proof.
+ Show Proof.
+ intro.
+ Show Proof.
+ omega.
+ Show Proof.
+ Qed.
